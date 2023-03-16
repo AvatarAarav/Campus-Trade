080ea5e289f6b84540162fe051b689bc8b55d1f6
AvatarAarav <aarav.n21@iiits.in>
2023-03-16 15:44:24 +0530

    Adding loggin api

--- Router/apiRouter.js
+++ Router/apiRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import multer from "multer";
 import { submitForm } from "../controllers/submitForm.js";
-import { AddUser } from "../db/DB_Functions.js";
+import { addUserAPI } from "../controllers/register.js";
+import { checkLogin } from "../db/DB_Functions.js";
 import { getAllProductsAPI } from "../controllers/apiFunctions.js";
+
 const router=express.Router();
 
 
@@ -11,5 +13,6 @@ router.get('/products',getAllProductsAPI)
 // Set up the multer middleware to handle file uploads
 const upload = multer({ dest: 'uploads/' });
 router.post('/form',upload.single('photo'),submitForm)
-router.post('/user/register',AddUser)
+router.post('/user/register',addUserAPI)
+router.post('user/login',checkLogin)
 export default router
