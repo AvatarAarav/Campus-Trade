71abab6cee77d18c5b630f1c3ddf0f6969f6b927
AvatarAarav <aarav.n21@iiits.in>
2023-03-18 23:03:37 +0530

    adding rendering funcs

--- controllers/login.js
+++ controllers/login.js
@@ -3,7 +3,7 @@ export const checkLoginAPI=async (req,res)=>{
     try {
         // console.log(req.body)
         const userData=await checkLogin(req.body)
-        res.status(200).json(userData);
+        res.status(200).render('index');
     } catch (error) {
         console.error(`${error.message}!!`)
         res.status(404).send(`${error.message}!!`)
